e2b9478413a68cef43ec8574523a6649e2c499a0
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-11-15 19:06:11 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,7 +9,7 @@ class EmailParser
   end 
   def parse 
       if @emails.match(/,/)
-        return @emails.split(/,\s/).uniq
+        return @emails.split(/[,\s]/).uniq
       else 
         return @emails.split(" ").uniq
       end 
